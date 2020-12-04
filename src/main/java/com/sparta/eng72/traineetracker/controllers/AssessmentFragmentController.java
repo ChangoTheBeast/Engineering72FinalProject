27e8375df9cdc60fd97d2cade32c8ffbda623842
shahidenayat <shahid.enayat@gmail.com>
2020-12-04 15:14:58 +0000

    update

---
+++ src/main/java/com/sparta/eng72/traineetracker/controllers/AssessmentFragmentController.java
@@ -0,0 +1,14 @@
+package com.sparta.eng72.traineetracker.controllers;
+
+import com.sparta.eng72.traineetracker.services.CodingGamesAPIService;
+import com.sparta.eng72.traineetracker.services.TraineeService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AssessmentFragmentController {
+
+    private CodingGamesAPIService codingGamesAPIService;
+    private TraineeService traineeService;
+    private
+
+}
